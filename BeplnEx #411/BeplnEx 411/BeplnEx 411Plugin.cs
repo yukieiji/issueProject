36b0b1aa4ee1140f6110e9033fe1ad8600bbfdd0
yukieiji <yukieiji@users.noreply.github.com>
2022-05-06 15:17:24 +0900

    feat:beplnEx 411 issue project

---
+++ BeplnEx #411/BeplnEx 411/BeplnEx 411Plugin.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+
+using HarmonyLib;
+
+
+namespace ExtremeRoles
+{
+
+    [BepInAutoPlugin("me.yukieiji.test", "BeplnEx 411")]
+    [BepInProcess("Among Us.exe")]
+    public partial class BeplnExPlugin : BasePlugin
+    {
+        public Harmony Harmony { get; } = new Harmony(Id);
+
+        internal static BepInEx.Logging.ManualLogSource Logger;
+        public static ConfigEntry<bool> DebugMode { get; private set; }
+
+        public override void Load()
+        {
+            Logger = Log;
+            Harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
+    public static class MainMenuManagerStartPatch
+    {
+        public static void Postfix()
+        {
+            BeplnExPlugin.Logger.LogInfo(
+                TranslationController.Instance.GetString(StringNames.Tasks));
+        }
+    }
+
+}
