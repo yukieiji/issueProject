a2d2643b6927ca7473d198db302e9c18fa9b3ab0
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-06-16 14:18:50 +0900

    feat:new issues class

---
+++ BeplnEx_CantPatchGenericClass/BeplnEx_CantPatchGenericClass/plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+
+using HarmonyLib;
+
+
+namespace IssueProject;
+
+
+[BepInAutoPlugin("me.yukieiji.test", "BeplnEx.CantPatchGenericClass")]
+[BepInProcess("Among Us.exe")]
+public partial class BeplnExPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new Harmony(Id);
+
+    internal static BepInEx.Logging.ManualLogSource Logger;
+    public static ConfigEntry<bool> DebugMode { get; private set; }
+
+    public override void Load()
+    {
+        Logger = Log;
+        Harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(
+    typeof(DestroyableSingleton<HudManager>),
+    nameof(DestroyableSingleton<HudManager>.Instance),
+    MethodType.Getter)]
+public static class TestPatch
+{
+    public static bool Prefix(DestroyableSingleton<HudManager> __instance)
+    {
+        return true;
+    }
+}
