50fd9dc58df1eb5fdefef75510468da4acea0521
yukieiji <yuki31gakki97@yahoo.co.jp>
2023-07-19 19:22:05 +0900

    feat:Patch

---
+++ BeplnEx_SameOverrideMethodPatch/BeplnEx_SameOverrideMethodPatch/plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+
+using HarmonyLib;
+
+namespace IssueProject;
+
+
+[BepInAutoPlugin("me.yukieiji.test", "BeplnEx.SameOverrideMethodPatch")]
+[BepInProcess("Among Us.exe")]
+public partial class BeplnExPlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new Harmony(Id);
+
+    internal static BepInEx.Logging.ManualLogSource Logger;
+    public static ConfigEntry<bool> DebugMode { get; private set; }
+
+    public override void Load()
+    {
+        Logger = Log;
+        Harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(HatData), nameof(HatData.CreateAddressableAsset))]
+public static class HatDataPatch
+{
+    public static bool Prefix()
+    {
+        BeplnExPlugin.Logger.LogInfo("HatData Called!!");
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(VisorData), nameof(VisorData.CreateAddressableAsset))]
+public static class VisorDataPatch
+{
+    public static bool Prefix()
+    {
+        BeplnExPlugin.Logger.LogInfo("VisorData Called!!");
+        return true;
+    }
+}
+
+[HarmonyPatch(typeof(NamePlateData), nameof(NamePlateData.CreateAddressableAsset))]
+public static class NamePlateDataPatch
+{
+    public static bool Prefix()
+    {
+        BeplnExPlugin.Logger.LogInfo("NamePlateData Called!!");
+        return true;
+    }
+}
