2cbfce804da0b9ee7a3b703624d1dc43f1a28052
yukieiji <yuki31gakki97@yahoo.co.jp>
2025-05-22 12:15:15 +0900

    feat : error proj

---
+++ AmongUs_MsBuild/Sample/Class1.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+
+using HarmonyLib;
+
+namespace Sample;
+
+[BepInAutoPlugin("me.yukieiji.sample", "sample")]
+[BepInProcess("Among Us.exe")]
+public partial class ExtremeRolesPlugin : BasePlugin
+{
+    public override void Load()
+    {
+
+    }
+}
